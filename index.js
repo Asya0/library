ddd3ce45911df24f21bc94c1a066d10768abbf7a
Anna Gerasimenko <annagerasimenko@MacBook-Pro-Anuar.local>
2023-10-08 22:53:32 +0600

    feat: added close popup

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const myLibrary = ["The Hobbit", "A Game of Thrones"];
 function Book(title) {
     this.title = title;
     this.info = function () {
-        // console.log(title)
     }
     function addBookToLibrary() {
         let btn = document.querySelector(".addBook")
@@ -15,8 +14,28 @@ function Book(title) {
         console.log(item)
     }
 }
-Book()
-// const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295");
-// const gameOfThrones = new Book("A Game of Thrones", "George R. R. Martin", "694");
-// theHobbit.info()
-// psyMoney.info()
+Book();
+let btnClick = document.getElementsByClassName("confetti-button");
+const cardsContainer = document.querySelector('.cards');
+const cards = cardsContainer.querySelectorAll('.card');
+for (let i = 0; i < btnClick.length; i++) {
+    btnClick[i].addEventListener("click", () => {
+        let canvas = document.createElement("canvas");
+        let container = document.getElementsByClassName("button-wrapper")[0];
+        canvas.width = 600;
+        canvas.height = 600;
+        container.appendChild(canvas);
+
+        let confetti_button = confetti.create(canvas);
+        confetti_button().then(() => container.removeChild(canvas));
+    });
+}
+function closePopup() {
+    let addBook = document.querySelector(".add-book-popup");
+    let closeIcon = document.querySelector('.close-popup');
+    closeIcon.addEventListener("click", () => {
+        addBook.style.display = "none";
+    })
+}
+closePopup()
+
