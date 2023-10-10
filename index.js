df7e3fbe8d4ee2dfa2c9b4c91eb8917fc750bc65
Anna Gerasimenko <annagerasimenko@MacBook-Pro-Anuar.local>
2023-10-11 00:20:36 +0600

    feat: function of adding a book to the library

--- index.js
+++ index.js
@@ -1,52 +1,114 @@
-let body = document.querySelector(".body-wrapper");
-const myLibrary = ["The Hobbit", "A Game of Thrones"];
-
-function Book(title) {
-    this.title = title;
-    this.info = function () {
-    }
-    function addBookToLibrary() {
-        let btn = document.querySelector(".addBook")
-        let addBook = "lalal"
-        myLibrary.push(addBook);
-    }
-    addBookToLibrary()
-    for (item of myLibrary) {
-        console.log(item)
-    }
-}
-Book();
-let btnClick = document.getElementsByClassName("confetti-button");
+const books = [
+    {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        page: 295
+    },
+    {
+        title: "Atomic Habits",
+        author: "James Clear",
+        page: 288
+    },
+    {
+        title: "Eat That Frog",
+        author: "Brian Tracy",
+        page: 144
+    },
+    // {
+    //     title: "",
+    //     author: "",
+    //     page: 
+    // },
+]
+const body = document.querySelector(".body-wrapper");
+let popupAddBook = document.querySelector(".add-book-popup");
 const cardsContainer = document.querySelector('.cards');
 const cards = cardsContainer.querySelectorAll('.card');
-for (let i = 0; i < btnClick.length; i++) {
-    btnClick[i].addEventListener("click", () => {
-        let canvas = document.createElement("canvas");
-        let container = document.getElementsByClassName("button-wrapper")[0];
-        canvas.width = 600;
-        canvas.height = 600;
-        container.appendChild(canvas);
 
-        let confetti_button = confetti.create(canvas);
-        confetti_button().then(() => container.removeChild(canvas));
-    });
+function createBookCard() {
+    const cards = document.querySelector(".cards")
+    cards.innerHTML = "";
+
+    for (let i = 0; i < books.length; i++) {
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('card');
+        bookCard.innerHTML = `
+        <span class="card-title">${books[i].title} </span>
+        <span class=" card-subtitle">by ${books[i].author} </span>
+        <span class="card-pages">pages: ${books[i].page}</span>
+        <div class="edit"></div> `;
+        cards.appendChild(bookCard);
+    }
 }
-let addBook = document.querySelector(".add-book-popup");
-function closePopup() {
-    let closeIcon = document.querySelector('.close-popup');
-    closeIcon.addEventListener("click", () => {
-        addBook.style.display = "none";
-        document.body.style.filter = "none";
-    })
+function showConfetti() {
+    const btnClick = document.getElementsByClassName("confetti-button");
+
+    for (let i = 0; i < btnClick.length; i++) {
+        btnClick[i].addEventListener("click", () => {
+            let canvas = document.createElement("canvas");
+            let container = document.getElementsByClassName("button-wrapper")[0];
+            let confetti_button = confetti.create(canvas);
+            canvas.width = 600;
+            canvas.height = 600;
+            container.appendChild(canvas);
+            confetti_button().then(() => container.removeChild(canvas));
+        });
+    }
 }
 function openPopup() {
     let openIcon = document.querySelector(".add-book-btn");
     openIcon.addEventListener("click", () => {
-        addBook.style.display = "block";
-        // if (addBook.style.display === "block") {
-        //     document.body.style.filter = "blur(5px)";
-        // }
+        popupAddBook.style.display = "block";
     })
 }
-closePopup()
-openPopup()
+function closePopup() {
+    let closeIcon = document.querySelector('.close-popup');
+    closeIcon.addEventListener("click", () => {
+        popupAddBook.style.display = "none";
+        document.body.style.filter = "none";
+    })
+}
+// let click = body.addEventListener("click", function (e) {
+//     console.log(e.target)
+// })
+let newTitle = document.querySelector("#title")
+let newAuthor = document.querySelector("#author")
+let newPage = document.querySelector("#pages")
+let submitBook = document.querySelector("#submitBook")
+
+function addNewBook() {
+    submitBook.addEventListener("click", function () {
+        const newTitle = document.getElementById("title").value;
+        const newAuthor = document.getElementById("author").value;
+        const newPage = document.getElementById("pages").value;
+        // console.log(newTitle)
+
+
+        if (newTitle !== "" && newAuthor !== "" && newPage !== "") {
+            const newBook = {
+                title: newTitle,
+                author: newAuthor,
+                page: newPage
+            };
+
+            books.push(newBook);
+
+            // Очищаем поля ввода
+            document.getElementById("title").value = "";
+            document.getElementById("author").value = "";
+            document.getElementById("pages").value = "";
+
+            //проблема здесь. ничего не обновляется
+            createBookCard();
+
+        }
+    });
+}
+
+
+
+createBookCard();
+showConfetti();
+closePopup();
+openPopup();
+addNewBook();
